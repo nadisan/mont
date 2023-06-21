187485be420db07b7ddd836b7c21651a18cd3344
Nadia <nadiawellela@gmail.com>
2023-06-20 22:01:17 -0700

    main.c

--- main.c
+++ main.c
@@ -3,85 +3,86 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * push - add int to stack as header
+ * @stack: stack linked list
+ * @line_number: number to insert on stack
+ *
+ */
+
 void push(stack_t **stack, unsigned int line_number)
 {
-    int value = line_number;
+	int value = line_number;
+	stack_t *new_node = malloc(sizeof(stack_t));
 
-    stack_t *new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
-    if (*stack != NULL)
-        (*stack)->prev = new_node;
-    *stack = new_node;
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+	*stack = new_node;
 }
 
-void pall(stack_t **stack)
+/**
+ * pall - prints all stack elements
+ * @stack: stack linked list
+ * @line_number: number to insert on stack
+ *
+ */
+
+void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *current = *stack;
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	stack_t *current = *stack;
+	unsigned int i = 0;
+
+	while (current != NULL && i <= line_number)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		i++;
+	}
 }
+/**
+ * main- executes command from argument
+ * @argc: number of command
+ * @argv: operation and command file
+ * Return: 0 on success
+ *
+ */
 
 int main(int argc, char *argv[])
 {
-   FILE * pFile;
-   char buffer [100];
-   stack_t *stack = NULL;
-   /*char *opcode = NULL;	*/
-   unsigned int line_number = 0;
-   char *fun;
-   
-   
+	FILE *pFile;
+	char buffer[100];
+	stack_t *stack = NULL;
+	/*char *opcode = NULL;	*/
+	unsigned int line_number = 0;
+	char *fun;
 
-   if (argc == 2)
-	   pFile = fopen (argv[1] , "r");
-   if (pFile == NULL) perror ("Error opening file");
-   else
-   {
-     while ( !feof (pFile) )
-     {
-	line_number++;
-       if ( fgets (buffer , 100 , pFile) == NULL ) break;
-       fun = strtok(buffer, " \n");
-	
-       if (strcmp(fun, "push") == 0)
-	       push(&stack, line_number);
-       if (strcmp(fun, "pall") == 0)
-	       pall(&stack);	
-     }
-     fclose (pFile);
-   }
-   return 0;
+	if (argc == 2)
+		pFile = fopen(argv[1], "r");
+	if (pFile == NULL)
+		perror("Error opening file");
+	else
+	{
+		while (!feof(pFile))
+		{
+			line_number++;
+			if (fgets(buffer, 100, pFile) == NULL)
+				break;
+			fun = strtok(buffer, " \n");
+
+			if (strcmp(fun, "push") == 0;
+				push(&stack, line_number);
+			if (strcmp(fun, "pall") == 0;
+				pall(&stack);
+		}
+		fclose(pFile);
+	}
+	return (0);
 }
-
-/*int main(int argc, char *argv[])
-{
-    stack_t *stack = NULL;
-    char *opcode = NULL;
-    size_t line_size = 1000;
-    unsigned int line_number = 0;
-    while (getline(&opcode, &line_size, argv[1]) != -1)
-    {
-        line_number++;
-        instruction_t *instruction = argv[0];
-        if (instruction == NULL)
-        {
-            fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-            exit(EXIT_FAILURE);
-        }
-        instruction->f(&stack, line_number);
-    }
-
-    free(stack);
-
-    return 0;
-}*/
